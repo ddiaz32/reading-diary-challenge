5335a72ecd9d190e815af31cd2f0f1c148756bf5
David Diaz <daviddiaz3233@gmail.com>
2026-03-09 17:05:09 -0500

    Add class Book and method constructor

--- readingdiary/model.py
+++ readingdiary/model.py
@@ -10,3 +10,22 @@ class Note:
 
     def __str__(self):
         return f"{self.date} - page {self.page}: {self.text}"
+    
+
+class Book:
+    EXCELLENT = 3
+    GOOD = 2
+    BAD = 1
+    UNRATED = -1
+
+    def __init__(self, isbn: str, title: str, author: str, pages: int):
+        self.isbn = isbn
+        self.title = title
+        self.author = author
+        self.pages = pages
+        self.rating = Book.UNRATED
+        self.notes = []
+
+
+  
+
