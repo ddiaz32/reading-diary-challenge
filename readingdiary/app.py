4da1d85ce1d3ca6a57f2394ea2fbcdd28048736e
David Diaz <daviddiaz3233@gmail.com>
2026-03-09 16:59:01 -0500

    Add class Note

--- readingdiary/app.py
+++ readingdiary/app.py
@@ -1,4 +1,7 @@
 from readingdiary.view import UIConsole
+from datetime import datetime
+
+
 
 
 def main():
@@ -8,3 +11,13 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+
+class Note:
+    def __init__(self, text: str, page: int, date:datetime):
+        self.text = text
+        self.page = page
+        self.date = date 
+
+    def __str__(self):
+        return f"{self.date} - page {self.page}: {self.text}"
--- readingdiary/model.py
+++ readingdiary/model.py
@@ -1,3 +1,12 @@
 from datetime import datetime
 
 # TODO: Add code here
+
+class Note:
+    def __init__(self, text: str, page: int, date:datetime):
+        self.text = text
+        self.page = page
+        self.date = date 
+
+    def __str__(self):
+        return f"{self.date} - page {self.page}: {self.text}"
